79be599d0d7ed3737ea7462a7aaa4c4154c3824d
Ilya <ayupow450106@gmail.com>
2020-05-04 11:44:14 +0500

    reformat files and update readme

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import random
 import string
@@ -32,7 +33,7 @@ def from_file(repo: Repository, file: str):
 
 
 def from_list(repo: Repository, commits: List[str]):
-    tmp = f"tmp-{random_str(40)}"                   #запишем строки во временный файл
+    tmp = f"tmp-{random_str(40)}"  # запишем строки во временный файл
     file = open(tmp, 'w')
     for i in commits:
         hash_id = repo.get_hash(i)
@@ -41,11 +42,12 @@ def from_list(repo: Repository, commits: List[str]):
             exit(1)
         file.write(f"{hash_id[:7]} {repo.get_comment(hash_id)}\n")
     file.close()
-    os.system(f"$EDITOR {tmp}")                     #вызовем редактор по умолчанию
-    from_file(repo, tmp)                            #вызываем изменения из файла
-    os.remove(tmp)                                  #удаляем файл
+    os.system(f"$EDITOR {tmp}")  # вызовем редактор по умолчанию
+    from_file(repo, tmp)  # вызываем изменения из файла
+    os.remove(tmp)  # удаляем файл
 
-#тут всё просто
+
+# тут всё просто
 def from_cli(repo: Repository, hash_str: str, comment: str):
     hash_id = repo.get_hash(hash_str)
     if hash_id == -1:
@@ -53,6 +55,39 @@ def from_cli(repo: Repository, hash_str: str, comment: str):
         exit(1)
     repo.change_commits([hash_id], [comment])
 
+
+parser = argparse.ArgumentParser(description='Fast reword for git')
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(0)
+arguments = sys.argv[1:]
+hash_id = None
+comment = ""
+parser.add_argument('-f', action="store", dest="file_path", help="path to file with list of changes")
+parser.add_argument('-c', action="store", dest="directory", default="./", type=str, help="path to repo directory")
+parser.add_argument('-l', dest="commit_list", nargs='*', help="commits list to reword")
+if len(arguments) > 1:
+    if arguments[0][0] != '-' and arguments[1][0] != '-':
+        hash_id = arguments[0]
+        comment = arguments[1]
+        if len(arguments) > 2:
+            arguments = arguments[2:]
+        else:
+            arguments = []
+
+args = parser.parse_args(arguments)
+repo = Repository(args.directory + '/' * (args.directory[-1] != '/'))
+if not hash_id is None:
+    from_cli(repo, hash_id, comment)
+    exit(0)
+if not args.file_path is None:
+    from_file(repo, args.file_path)
+    exit(0)
+if len(args.commit_list) > 0:
+    from_list(repo, args.commit_list)
+    exit(0)
+print("Not enough arguments", file=sys.stderr)
+exit(1)
 
 # икона от репозиториев с SHA-256 вместо SHA-1
 # .______________________________________________.
@@ -84,32 +119,3 @@ def from_cli(repo: Repository, hash_str: str, comment: str):
 # | Y    ,'/    _<   /  \\\\  | jj   / ~-_       |
 # | |   / f c-~~  ~-<____UUU--~~~_--~     \      |
 # |_|__f__|__````-----'_________/__________?_____|
-arguments = sys.argv[1:]
-parser = argparse.ArgumentParser(description='Fast reword for git')
-hash_id = None
-comment = ""
-parser.add_argument('-f', action="store", dest="file_path", help="path to file with list of changes")
-parser.add_argument('-c', action="store", dest="directory", default="./", type=str,help="path to repo directory")
-parser.add_argument('-l',dest="commit_list", nargs='*', help="commits list to reword")
-if len(arguments) > 1:
-    if arguments[0][0] != '-' and arguments[1][0] != '-':
-        hash_id=arguments[0]
-        comment=arguments[1]
-        if len(arguments) > 2:
-            arguments = arguments[2:]
-        else:
-            arguments = []
-
-args = parser.parse_args(arguments)
-repo=Repository(args.directory+'/'*(args.directory[-1]!='/'))
-if not hash_id is None:
-    from_cli(repo,hash_id,comment)
-    exit(0)
-if not args.file_path is None:
-    from_file(repo,args.file_path)
-    exit(0)
-if len(args.commit_list)>0:
-    from_list(repo,args.commit_list)
-    exit(0)
-print("Not enough arguments",file=sys.stderr)
-exit(1)
